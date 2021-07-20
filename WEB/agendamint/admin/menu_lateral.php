2992168a67ac1db619dbcebc154fa9eeebf7fb1e
Eduardo Andrade <tvduardo@gmail.com>
2021-07-20 12:08:44 -0300

    tirando permissão de edição em dados da empresa do funcionario

--- WEB/agendamint/admin/menu_lateral.php
+++ WEB/agendamint/admin/menu_lateral.php
@@ -22,9 +22,11 @@
             <li><a class="h5" href="principal.php?link=3">Funcionários</a></li>
         </ul>
     <?php } ?>
+    <?php if($_SESSION['nivel_acesso'] == USER_MASTER){ ?>
     <div class="footer-menu-lateral">
         <a class="h5" href="principal.php?link=6"><span class="bi bi-gear"></span> Dados da Empresa</a>
     </div>
+        <?php } ?>
     
 </div>
 
